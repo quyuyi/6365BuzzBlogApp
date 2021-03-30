5576fbf884dfb64bb231388ba9c2af617649a95f
quyuyi <quyuyi@umich.edu>
2021-03-30 17:30:10 -0400

    update gitignore

--- app/recommendation/database/dataset.js
+++
@@ -1,6 +0,0 @@
-var dataset = 
-[
-    {"post_id":NumberInt(1), "tweet_id":"123456789", "created_at":NumberInt(123), "text":"Hello world!", "keywords":["hello","world"]},
-    {"post_id":NumberInt(2), "tweet_id":"987654321", "created_at":NumberInt(456), "text":"Good Morning!", "keywords":["good","morning"]},
-    {"post_id":NumberInt(3), "tweet_id":"123321123", "created_at":NumberInt(789), "text":"Have a good day", "keywords":["good"]}
-]
